fedab4247f7bae9810970ad2554afae15ea99ddd
CallumAttryde <callumattryde@gmail.com>
2017-12-04 14:51:29 +0000

    adding SAG apache2 release stanzas and use APAMA_HOME rather than APAMA_WORK

--- tests/system/RegexCodec_sys_001/run.py
+++ tests/system/RegexCodec_sys_001/run.py
@@ -1,11 +1,15 @@
+# Copyright (c) 2017 Software AG, Darmstadt, Germany and/or its licensors 
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 from pysys.basetest import BaseTest
 from apama.correlator import CorrelatorHelper
 
 class PySysTest(BaseTest):
 
 	def execute(self):
-		# since we can't specify locations other than APAMA_WORK/HOME and cwd, 
-		# have to copy input file to output dir
 		import shutil
 		for f in ['input.txt']:
 			shutil.copyfile(self.input+'/'+f, self.output+'/'+f)
