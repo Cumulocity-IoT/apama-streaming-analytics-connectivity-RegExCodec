215c9edbac159692b467e8eabc031f3500d40b33
CaribouJohn <john@heath.mx>
2018-10-15 13:20:49 +0100

    10.3 release changes

--- src/com/softwareag/samples/RegexCodec.java
+++ src/com/softwareag/samples/RegexCodec.java
@@ -1,11 +1,6 @@
 /*
- * Copyright © 2017 Software AG, Darmstadt, Germany and/or its licensors
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ * $Copyright (c) 2016-2017 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA, and/or its subsidiaries and/or its affiliates and/or their licensors.$
+ * Use, reproduction, transfer, publication or disclosure is prohibited except as specifically provided for in your License Agreement with Software AG
  */
 
 package com.softwareag.samples;
@@ -18,10 +13,12 @@ import java.util.Map;
 import java.util.Set;
 import java.util.regex.Pattern;
 
+
 import com.softwareag.connectivity.AbstractSimpleCodec;
 import com.softwareag.connectivity.Message;
 import com.softwareag.connectivity.PluginConstructorParameters.CodecConstructorParameters;
 import com.softwareag.connectivity.util.MapExtractor;
+
 
 /**
  * Performs a single regular expression replacement/substitution on strings in one or more  
@@ -233,4 +230,5 @@ public class RegexCodec extends AbstractSimpleCodec {
 	
 	/** Identifies the version of the API this plug-in was built against. */
 	public static final String CONNECTIVITY_API_VERSION = com.softwareag.connectivity.ConnectivityPlugin.CONNECTIVITY_API_VERSION;
+
 }
--- tests/system/RegexCodec_sys_002/run.py
+++ tests/system/RegexCodec_sys_002/run.py
@@ -1,16 +1,15 @@
-# Copyright (c) 2017 Software AG, Darmstadt, Germany and/or its licensors 
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 # Sample PySys testcase
+# Copyright (c) 2016 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA, and/or its subsidiaries and/or its affiliates and/or their licensors. 
+# Use, reproduction, transfer, publication or disclosure is prohibited except as specifically provided for in your License Agreement with Software AG 
+
 from pysys.basetest import BaseTest
 from apama.correlator import CorrelatorHelper
 
 class PySysTest(BaseTest):
 
 	def execute(self):
+		# since we can't specify locations other than APAMA_WORK/HOME and cwd, 
+		# have to copy input file to output dir
 		import shutil
 		for f in ['input.txt']:
 			shutil.copyfile(self.input+'/'+f, self.output+'/'+f)
